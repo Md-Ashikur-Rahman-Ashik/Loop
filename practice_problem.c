788565dcde0ced7835f27b1b830f6507f750c05a
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-04-09 11:55:16 +0600

    Solved second problem

--- practice_problem.c
+++ practice_problem.c
@@ -1,16 +1,31 @@
+// #include <stdio.h>
+
+// int main()
+// {
+//     int i, j;
+
+//     for (i = 0; i <= 5; i++)
+//     {
+//         for (j = 1; j < 3; j++)
+//         {
+//             printf("* = %d\n", i);
+//         }
+//     }
+
+//     return 0;
+// }
+
 #include <stdio.h>
 
 int main()
 {
-    int i, j;
+    int i = 0, j = 0;
 
-    for (i = 0; i <= 5; i++)
+    for (j = 1; j <= 5; j++)
     {
-        for (j = 1; j < 3; j++)
-        {
-            printf("* = %d\n", i);
-        }
+        /* code */
+        i++;
     }
 
-    return 0;
+    printf("%d", j);
 }
