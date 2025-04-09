2ad9afeefaacc36c93bc1ccce0240793bf149b44
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-04-10 05:10:41 +0600

    Solved fourth problem of second practice day

--- practice_problem.c
+++ practice_problem.c
@@ -273,3 +273,20 @@
 
 //     return 0;
 // }
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int num, i;
+
+//     scanf("%d", &num);
+
+//     for (i = 1; i <= num; i++)
+//     {
+//         /* code */
+//         printf("I Love Practice\n");
+//     }
+
+//     return 0;
+// }
