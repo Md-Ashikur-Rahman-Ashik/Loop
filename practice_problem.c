76774cc47b527f0117d93955a9d6d5b77bc5890a
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-04-10 04:49:21 +0600

    Third problem solved on phitron online judge

--- practice_problem.c
+++ practice_problem.c
@@ -189,3 +189,28 @@
 
 //     return 0;
 // }
+
+#include <stdio.h>
+
+int main()
+{
+    int num1, num2;
+
+    scanf("%d %d", &num1, &num2);
+
+    if (num1 % num2 == 0)
+    {
+        printf("Yes");
+    }
+    else if (num2 % num1 == 0)
+    {
+        /* code */
+        printf("Yes");
+    }
+    else
+    {
+        printf("No");
+    }
+
+    return 0;
+}
