5642ee43bcaee7a3d07a0944dbff8c2d74e63ed6
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-04-09 09:39:28 +0600

    Printed even numbers from 100 to 1

--- more_for_loop.c
+++ more_for_loop.c
@@ -40,13 +40,27 @@
 //     return 0;
 // }
 
+// #include <stdio.h>
+
+// int main()
+// {
+//     int i;
+
+//     for (i = 99; i >= 1; i = i - 2)
+//     {
+//         printf("%d\n", i);
+//     }
+
+//     return 0;
+// }
+
 #include <stdio.h>
 
 int main()
 {
     int i;
 
-    for (i = 99; i >= 1; i = i - 2)
+    for (i = 100; i >= 1; i = i - 2)
     {
         printf("%d\n", i);
     }
