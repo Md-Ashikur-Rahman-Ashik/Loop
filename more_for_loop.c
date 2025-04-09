48e3ef20653d379ae3ae42f3315670375359b2bf
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-04-09 09:43:47 +0600

    Printed numbers from 2 to 100
    These numbers are printed as a multiplication of 2

--- more_for_loop.c
+++ more_for_loop.c
@@ -54,13 +54,27 @@
 //     return 0;
 // }
 
+// #include <stdio.h>
+
+// int main()
+// {
+//     int i;
+
+//     for (i = 100; i >= 1; i = i - 2)
+//     {
+//         printf("%d\n", i);
+//     }
+
+//     return 0;
+// }
+
 #include <stdio.h>
 
 int main()
 {
     int i;
 
-    for (i = 100; i >= 1; i = i - 2)
+    for (i = 2; i <= 100; i = i * 2)
     {
         printf("%d\n", i);
     }
