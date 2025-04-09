5b0ab29f48b898602e8ba7b542332655db9a6778
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-04-09 12:45:50 +0600

    Solved first phitron problem

--- practice_problem.c
+++ practice_problem.c
@@ -132,3 +132,43 @@
 
 //     return 0;
 // }
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int num;
+
+//     printf("Enter a number : ");
+//     scanf("%d", &num);
+
+//     if (num == 0)
+//     {
+//         printf("Zero");
+//     }
+//     else
+//     {
+//         printf("Non Zero");
+//     }
+
+//     return 0;
+// }
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     long long int num;
+//     scanf("%lld", &num);
+
+//     if (num == 0)
+//     {
+//         printf("Zero");
+//     }
+//     else
+//     {
+//         printf("Non Zero");
+//     }
+
+//     return 0;
+// }
