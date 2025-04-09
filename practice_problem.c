d131da6626fb4740aa87f0be96b365c7a269692a
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-04-09 11:59:29 +0600

    Solved fourth problem

--- practice_problem.c
+++ practice_problem.c
@@ -38,8 +38,23 @@
 //         /* code */
 //         int x = 4;
 //     }
-    
+
 //     printf("%d", i);
 
 //     return 0
+// }
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int i = 1;
+//     do
+//     {
+//         /* code */
+//         printf("%d", i);
+//         i++;
+//     } while (i < 5);
+
+//     return 0;
 // }
