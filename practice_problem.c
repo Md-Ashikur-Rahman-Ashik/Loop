5a2af00eab5cb924516d798a66aa10e844b683fa
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-04-10 04:59:52 +0600

    Solved second problem on second practice day

--- practice_problem.c
+++ practice_problem.c
@@ -236,3 +236,18 @@
 
 //     return 0;
 // }
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int num1, num2, sum;
+
+//     scanf("%d %d", &num1, &num2);
+
+//     sum = num1 + num2;
+
+//     printf("%d", sum);
+
+//     return 0;
+// }
