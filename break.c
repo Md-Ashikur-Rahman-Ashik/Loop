507fc59666abfaa11dccbf0617b70b21f607cdb5
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-04-09 10:55:05 +0600

    Skipped a certain number in a loop

--- break.c
+++ break.c
@@ -1,17 +1,36 @@
+// #include <stdio.h>
+
+// int main()
+// {
+//     int i;
+
+//     for (i = 1; i <= 100; i++)
+//     {
+//         printf("%d\n", i);
+
+//         if (i == 10)
+//         {
+//             break;
+//         }
+//     }
+
+//     return 0;
+// }
+
 #include <stdio.h>
 
 int main()
 {
     int i;
 
-    for (i = 1; i <= 100; i++)
+    for (i = 1; i <= 10; i++)
     {
-        printf("%d\n", i);
-
-        if (i == 10)
+        if (i == 5)
         {
-            break;
+            continue;
         }
+
+        printf("%d\n", i);
     }
 
     return 0;
