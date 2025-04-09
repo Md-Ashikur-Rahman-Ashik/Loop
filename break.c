f8eae751b2c44c55fd78b1abad92629e4b9abd50
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-04-09 10:40:08 +0600

    Paused a loop at a certain point

---
+++ break.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int i;
+
+    for (i = 1; i <= 100; i++)
+    {
+        printf("%d\n", i);
+
+        if (i == 10)
+        {
+            break;
+        }
+    }
+
+    return 0;
+}
