9ad731587ded1e44cd0a1f8e1d5c7dfd3b191caf
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-04-09 08:58:59 +0600

    Printed 1 to 1000 using loop

--- for_loop.c
+++ for_loop.c
@@ -1,12 +1,26 @@
+// #include <stdio.h>
+
+// int main()
+// {
+//     int i;
+
+//     for (i = 1; i <= 100; i++)
+//     {
+//         printf("I'm sorry - %d\n", i);
+//     }
+
+//     return 0;
+// }
+
 #include <stdio.h>
 
 int main()
 {
     int i;
 
-    for (i = 1; i <= 100; i++)
+    for (i = 1; i <= 1000; i++)
     {
-        printf("I'm sorry - %d\n", i);
+        printf("%d\n", i);
     }
 
     return 0;
